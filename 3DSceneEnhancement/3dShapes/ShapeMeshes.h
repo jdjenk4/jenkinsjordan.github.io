8340968ff913e74ccd77fd22d15327259aac093c
Jordan Jenkins <jordan.jenkins2@snhu.edu>
2025-10-06 17:39:48 -0400

    Restored missing 3DSceneEnhancement files and directories from branch-two

---
+++ 3DSceneEnhancement/3dShapes/ShapeMeshes.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <gl/glew.h>
+
+class ShapeMeshes
+{
+public:
+    ShapeMeshes();
+    ~ShapeMeshes();
+
+    void LoadPlaneMesh();
+    void LoadSphereMesh();
+    void LoadCylinderMesh();
+    void LoadTorusMesh();
+    void LoadTaperedCylinderMesh();
+    void LoadHalfSphereMesh();
+
+    void DrawPlaneMesh();
+    void DrawSphereMesh();
+    void DrawCylinderMesh();
+    void DrawTorusMesh();
+    void DrawTaperedCylinderMesh();
+    void DrawHalfSphereMesh();
+
+private:
+    GLuint planeVAO, sphereVAO, cylinderVAO, torusVAO, taperedCylinderVAO, halfSphereVAO, dummyVAO;
+    // + VBO/EBO as needed
+};
